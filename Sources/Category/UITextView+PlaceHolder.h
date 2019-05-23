fdc4607ec00343de3b5b29fedc1d5b6231690a46
luowei <luowei@cqyanyu.com>
2019-05-23 10:44:33 +0800

    添加textview placeholder

---
+++ Sources/Category/UITextView+PlaceHolder.h
@@ -0,0 +1,17 @@
+//
+//  UITextView+PlaceHolder.h
+//  
+//
+//  Created by simple on 2018/8/13.
+//  Copyright © 2019年 luowei. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface UITextView (PlaceHolder)
+
+@property (nonatomic, copy) NSString *placeHolder;
+@property (nonatomic, copy) UIColor *placeHolderColor;
+
+@end
---
+++ Sources/Category/UITextView+PlaceHolder.m
@@ -0,0 +1,70 @@
+//
+//  UITextView+PlaceHolder.m
+//  
+//
+//  Created by simple on 2018/8/13.
+//  Copyright © 2019年 luowei. All rights reserved.
+//
+
+#import "UITextView+PlaceHolder.h"
+#import <objc/runtime.h>
+
+#define kScreenW [UIScreen mainScreen].bounds.size.width
+static const void *textView_key = @"placeHolder";
+static const void *textView_Color_key = @"placeHolderColor";
+
+@interface UITextView ()
+@end
+
+@implementation UITextView (PlaceHolder)
+
+- (void)setPlaceHolder:(NSString *)placeHolder
+{
+    if (placeHolder != self.placeHolder) {
+        objc_setAssociatedObject(self, textView_key, placeHolder, OBJC_ASSOCIATION_COPY_NONATOMIC);
+        
+        UILabel *placeHolderLb = [[UILabel alloc] initWithFrame:CGRectMake(2, 7, kScreenW-2*16, 21)];
+        placeHolderLb.tag = 10000;
+        placeHolderLb.contentMode = UIViewContentModeTop;
+        placeHolderLb.numberOfLines = 0;
+        placeHolderLb.textColor = self.placeHolderColor ? : [UIColor lightGrayColor];
+        placeHolderLb.font = [UIFont systemFontOfSize:16];
+        placeHolderLb.alpha = 1;
+        placeHolderLb.text = placeHolder;
+        [self addSubview:placeHolderLb];
+        
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textViewChanged:) name:UITextViewTextDidChangeNotification object:nil];
+    }
+}
+
+- (NSString *)placeHolder
+{
+    return objc_getAssociatedObject(self, textView_key);
+}
+
+- (void)setPlaceHolderColor:(UIColor *)placeHolderColor{
+    if (placeHolderColor != self.placeHolderColor) {
+        objc_setAssociatedObject(self, textView_Color_key, placeHolderColor, OBJC_ASSOCIATION_COPY_NONATOMIC);
+        UILabel *lb = [self viewWithTag:10000];
+        if (lb) {
+            lb.textColor = placeHolderColor;
+        }
+    }
+}
+
+- (UIColor *)placeHolderColor{
+    return objc_getAssociatedObject(self, textView_Color_key);
+}
+
+- (void)textViewChanged:(NSNotification *)noti
+{
+    UILabel *label = [self viewWithTag:10000];
+    if (self.text.length == 0) {
+        label.alpha = 1;
+    } else {
+        label.alpha = 0;
+    }
+}
+
+@end
